c4ae60e2bf310150dad35ef9dfb7b16b15ff9f4c
jerry861200 <jerry861200@gmail.com>
2024-10-25 12:48:40 -0700

    feat: make api template

--- src/layouts/InputPage/Components/UploadForm.tsx
+++ src/layouts/InputPage/Components/UploadForm.tsx
@@ -1,6 +1,39 @@
+import { MouseEventHandler } from "react";
+
 import { CheckBox } from "./CheckBox"
 
 export const UploadForm = () => {
+
+    const postText: MouseEventHandler = async (e) => {
+        e.preventDefault()
+        
+        try {
+            // TODO: change API path
+          const response = await fetch('https://jsonplaceholder.typicode.com/postst', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            // TODO:
+            body: JSON.stringify({title: 'foo',
+                body: 'bar',
+                userId: 1}),  // 将文本内容包装到请求主体中
+          });
+      
+          if (!response.ok) {
+            throw new Error('Failed to post data');
+          }
+      
+          const data = await response.json();
+          console.log('Data submitted successfully:', data);
+          return data;  // 返回响应数据，方便其他地方使用
+        } catch (error) {
+          console.error('Error posting data:', error);
+        }
+    
+    };
+
+    
     return (
         <div className="m-3 col-6">
             <form>
@@ -19,7 +52,7 @@ export const UploadForm = () => {
                             <input className="form-control rounded-3 shadow-sm" type="file" id="formFile" />
                         </div>
                         <div className="d-flex justify-content-center">
-                            <button type="submit" className="btn mt-3 text-light border-light fw-bold" style={{ borderWidth: '2px' }}>
+                            <button className="btn mt-3 text-light border-light fw-bold" style={{ borderWidth: '2px' }} onClick={postText}>
                                 Generate a response
                             </button>
                         </div>
