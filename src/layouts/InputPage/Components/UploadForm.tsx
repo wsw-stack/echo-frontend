db92fb13e063e14e0a0efe911359e3d4550d6355
Ahmed Najiub <56661044+ahmedryasser@users.noreply.github.com>
2024-10-25 19:01:14 -0700

    adds upload handler and textarea

--- src/layouts/InputPage/Components/UploadForm.tsx
+++ src/layouts/InputPage/Components/UploadForm.tsx
@@ -1,16 +1,16 @@
-import { FormEventHandler, MouseEventHandler, useState } from "react";
+import { ChangeEventHandler, FormEventHandler, MouseEventHandler, useState } from "react";
 
 import { CheckBox } from "./CheckBox"
 import { useNavigate } from "react-router-dom";
 
 export const UploadForm = () => {
     const navigate = useNavigate();  // 使用 useNavigate 钩子
-
+    const [text, setText] = useState<string>('');
     const postText: FormEventHandler<HTMLFormElement> = async (e) => {
         e.preventDefault();
         
         try {
-            const response = await fetch('http://127.0.0.1:5050/text', {
+            const response = await fetch('http://127.0.0.1:5000/generate', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -32,6 +32,26 @@ export const UploadForm = () => {
             console.error('Error posting data:', error);
         }
     };
+    // send to backend to change to text and put in the text area
+    const handleUpload: ChangeEventHandler<HTMLInputElement> = (e) => {
+        const file = (e.target as HTMLInputElement).files?.[0];
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+    
+            fetch('http://127.0.0.1:5000/to_text', {
+                method: 'POST',
+                body: formData,
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setText(data.text);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+    }
 
     
     return (
@@ -49,7 +69,8 @@ export const UploadForm = () => {
                         <div className="mb-3">
                             <p className="card-text fs-4 text-light lh-sm mb-1">Select a media file</p>
                             <p className="card-text fs-6 text-secondary">Supported types: .mp3 and .mp4, maximum 1GB</p>
-                            <input className="form-control rounded-3 shadow-sm" type="file" id="formFile" />
+                            <input className="form-control rounded-3 shadow-sm" onChange={(e) => handleUpload(e) } type="file" id="formFile" />
+                            <textarea className="form-control mt-3 rounded-3 shadow-sm" value={text} placeholder="Or paste your text here" rows={5}></textarea>
                         </div>
                         <div className="d-flex justify-content-center">
                             <button type="submit" className="btn mt-3 text-light border-light fw-bold" style={{ borderWidth: '2px' }}>
